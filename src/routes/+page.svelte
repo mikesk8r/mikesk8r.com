dae3f18bbdca26a3d1c87746c441688d96295e72
mikesk8r <michaelseguinlol@gmail.com>
2023-12-27 21:32:53 -0500

    More awesome content. Oh yeah baby!!!!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,11 @@
 <main>
     <h1>Welcome to mikesk8r.com!</h1>
-    <p>A dedicated website to all my silly projects and things. Have fun!</p>
+    <img src="https://web.archive.org/web/20090830230956/http://www.geocities.com/la_corsarie/welcome.gif" alt="Welcome to my site!">
+    <p>
+        A dedicated website to all my silly projects and things. 
+        You can click the links in the top bar to see different pages, or you can leave this site. 
+        I don't care.
+    </p>
+    <p>Have fun!</p>
     <h6>more coming soon! it'll be added later cuz im lazy *.*</h6>
 </main>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,7 +1,12 @@
 <main>
     <h1>About</h1>
-    <img width="320px" height="200px" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallpapercave.com%2Fwp%2FUzAi5VX.jpg&f=1&nofb=1&ipt=fbb5937863d9712aa1a343c69da8c4435f0286eeba948337818ffa5332020484&ipo=images/sleepy_cat.jpg" alt="Adorable sleepy cat.">
+    <abbr title="Aww! Sleepy kitty."><img
+        width="320px"
+        height="200px"
+        src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallpapercave.com%2Fwp%2FUzAi5VX.jpg&f=1&nofb=1&ipt=fbb5937863d9712aa1a343c69da8c4435f0286eeba948337818ffa5332020484&ipo=images/sleepy_cat.jpg"
+        alt="Adorable sleepy cat."
+    ></abbr>
     <p>I enjoy programming and games. And also computers, plus cats. And also, a million other things.</p>
     <h2>Links</h2>
-    <a href="https://steamcommunity.com/id/mike_sk8r">Steam profile</a>
+    <a href="https://steamcommunity.com/id/mike_sk8r" target="_blank">Steam profile</a>
 </main>
