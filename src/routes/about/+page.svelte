0a964ce20d4b26d546e7b3665db6bbf3dbe57e37
mikesk8r <michaelseguinlol@gmail.com>
2023-12-27 21:43:59 -0500

    Update: more coolness \(I need to stop.\)

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -6,6 +6,7 @@
         src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallpapercave.com%2Fwp%2FUzAi5VX.jpg&f=1&nofb=1&ipt=fbb5937863d9712aa1a343c69da8c4435f0286eeba948337818ffa5332020484&ipo=images/sleepy_cat.jpg"
         alt="Adorable sleepy cat."
     ></abbr>
+    <h6>Look at this cute little kitten! How could you say no to that face!</h6>
     <p>I enjoy programming and games. And also computers, plus cats. And also, a million other things.</p>
     <h2>Links</h2>
     <a href="https://steamcommunity.com/id/mike_sk8r" target="_blank">Steam profile</a>
