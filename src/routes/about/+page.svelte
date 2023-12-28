6fd76b9a1e6166cdab292113c2248189c8fa6adf
mikesk8r <michaelseguinlol@gmail.com>
2023-12-27 21:15:35 -0500

    Add cat picture

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,7 @@
 <main>
     <h1>About</h1>
+    <p>I enjoy programming and games. And also computers, plus cats. And also, a million other things.</p>
+    <h2>Links</h2>
+    <a href="https://discordapp.com/users/985655914193383464">Discord</a><br>
+    <a href="https://steamcommunity.com/id/mike_sk8r">Steam profile</a>
 </main>
