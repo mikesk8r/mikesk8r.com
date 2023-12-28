2f2e9faa37842e831d67eaff45b9a188a5e9f0aa
mikesk8r <michaelseguinlol@gmail.com>
2023-12-27 20:30:22 -0500

    Add basic nav + styles

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<nav>
+    <a href="/mikesk8r.com/">home</a>
+    <a href="/mikesk8r.com/about">about</a>
+    <a href="/mikesk8r.com/projects">projects</a>
+    <!-- <a href="/mikesk8r.com/"></a> -->
+</nav>
+
+<slot />
