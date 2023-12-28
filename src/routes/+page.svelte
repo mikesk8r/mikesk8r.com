4a14cfb64a7998051fb11fdc66232f86f4fccfc2
mikesk8r <michaelseguinlol@gmail.com>
2023-12-27 20:17:24 -0500

    Add silly basic content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,5 @@
-<h1>Welcome to mikesk8r.com</h1>
+<main>
+    <h1>Welcome to mikesk8r.com!</h1>
+    <p>A dedicated website to all my silly projects and things. Have fun!</p>
+    <h6>more coming soon! it'll be added later cuz im lazy *.*</h6>
+</main>
