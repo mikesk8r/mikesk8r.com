ae8a7a70ce88c042647bd08c3ae6cf8cf9fde67a
mikesk8r <michaelseguinlol@gmail.com>
2023-12-27 21:54:26 -0500

    New build

---
+++ docs/_app/immutable/chunks/paths.0HPwfGGL.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_tsggvm?.base??"/mikesk8r.com",t=globalThis.__sveltekit_tsggvm?.assets??s;export{t as a,s as b};
--- docs/_app/immutable/chunks/paths.B7FzLXY-.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_10i6are?.base??"/mikesk8r.com",e=globalThis.__sveltekit_10i6are?.assets??s;export{e as a,s as b};
