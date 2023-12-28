6a05ce8c956be52aaa90ecbc1a099eed122b9e2a
mikesk8r <michaelseguinlol@gmail.com>
2023-12-27 20:56:42 -0500

    Fix navbar hopefully for real this time

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,11 @@
+<script>
+    import { base } from '$app/paths';
+</script>
+
 <nav>
-    <!-- TODO: replace this horrible workaround -->
-    <a href="/">home</a>
-    <a href="/about">about</a>
-    <a href="/projects">projects</a>
+    <a href="{base}/">home</a>
+    <a href="{base}/about">about</a>
+    <a href="{base}/projects">projects</a>
     <!-- <a href=""></a> -->
 </nav>
 
