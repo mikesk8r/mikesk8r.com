82d3b4f0b1a2f7cace4bd814b972a114b3fe7194
mikesk8r <mike@compy.>
2024-04-07 00:52:28 -0400

    build

---
+++ docs/_app/immutable/chunks/paths.OCCW-4Sc.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_16wgzjm?.base??"/mikesk8r.com",e=globalThis.__sveltekit_16wgzjm?.assets??s;export{e as a,s as b};
--- docs/_app/immutable/chunks/paths.ODIwUbaL.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_j895ua?.base??"/mikesk8r.com",a=globalThis.__sveltekit_j895ua?.assets??s;export{a,s as b};
