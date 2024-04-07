cead7c7b0c520017ac8c2df4f0b2f37b1d9a622a
mikesk8r <mike@compy.>
2024-04-07 00:51:53 -0400

    edits

--- src/routes/about-me/+page.svelte
+++ src/routes/about-me/+page.svelte
@@ -1,12 +1,14 @@
+<script>
+    import { base } from '$app/paths';
+</script>
+
 <main>
     <h1>About me</h1>
-    <p>
-        As you could guess, I'm mikesk8r. I like computers, programming
+    <div>
+        As you could guess, I'm <p class="colored">mikesk8r</p> (a.k.a mike_sk8r). I like computers, programming
         (obviously, since I made this site), and also video games. You can also
          probably guess that I love cats. It should be pretty obvious.
-    </p>
-    <img src="stormy_bed.jpeg" alt="Stormy" width="504" height="378">
-    <h6>My cat, Stormy, laying on my bed. He is very fat. Much larger than my other cat.</h6>
+    </div>
     <p>Here are a few links you can check out if you want to. Feel free to send a friend request (where applicable).</p>
     <a href="https://steamcommunity.com/id/mike_sk8r/">My Steam profile</a>
 </main>
