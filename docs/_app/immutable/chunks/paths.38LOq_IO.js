128d9dce5005a84564ec86644225155bd7d79a3f
mikesk8r <mike@compy.>
2024-04-16 16:09:44 -0400

    oops forgor to build

--- docs/_app/immutable/chunks/paths.38LOq_IO.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1b4knvi?.base??"/mikesk8r.com",e=globalThis.__sveltekit_1b4knvi?.assets??s;export{e as a,s as b};
---
+++ docs/_app/immutable/chunks/paths.6vaSV_7J.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1apc9ee?.base??"/mikesk8r.com",e=globalThis.__sveltekit_1apc9ee?.assets??s;export{e as a,s as b};
